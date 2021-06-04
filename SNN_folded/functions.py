a813cd20e4a891344128d3db757ba79e702e75cc
galactico7 <52141963+galactico7@users.noreply.github.com>
2021-06-04 18:29:12 +0900

    Update functions.py

--- SNN_folded/functions.py
+++ SNN_folded/functions.py
@@ -1,24 +1,60 @@
 import torch
 import numpy as np
-import cupy as cp 
 
-# dtdu evaluation
-def dtdu(tau_m, tau_s, sp, um, us, grad_u):
-    temp = um/tau_m - us/tau_s
-    temp = cp.where(temp != 0, 1/temp, temp)
-    temp = cp.clip(temp, -100, 100) * sp.astype(float)
-    return grad_u + temp
+# define the gradient of spike timing with membrane potential
+class dtdu(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, u, um, us, thresh, tau_m, tau_s):
+        ctx.thresh = thresh
+        ctx.tau_m = tau_m 
+        ctx.tau_s = tau_s
+        ctx.save_for_backward(u, um, us)
+        return u.gt(thresh).float()
+
+# define the gradient of v with spike timing 
+class dvdt(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, v, x, tau, const):
+        v = v * np.exp(-1 / tau) + const * x
+        ctx.tau = tau
+        ctx.const = const
+        return v
+        
+# define output time function
+class dodt(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input_spike, input_time, num_steps):
+        ctx.save_for_backward(input_spike)
+        return input_spike * (input_time - num_steps)
+
+def dtdu_backward(um, us, sp, thresh, tau_m, tau_s):
+    temp = um / tau_m - us / tau_s
+    temp = torch.where(temp != 0, 1 / temp, temp)
+    dtdu = sp * torch.clamp(temp, -100, 100)
+    return dtdu
+
+# Cross-entropy loss
+def CEloss(outt, labels, batch_size):
+    smax = torch.exp(-outt) / torch.sum(torch.exp(-outt), 1, keepdim=True)
+    labels_ = torch.zeros(batch_size, 10).scatter_(1, labels.view(-1, 1), 1)
+    dldt = labels_ - smax
+    loss = - torch.mean(torch.sum(labels_ * torch.log(smax), 1))
+    return dldt, loss
 
 # NOSO model definition
-def noso(tau_m, tau_s, thresh, x, ref, vm, vs, w, sp):
+def noso(thresh, tau_m, tau_s, ops, x, sav, vm, vs, spike, outneuron=False):
     const = tau_m / (tau_m - tau_s)
-    decay_m, decay_s = np.exp(-1 / tau_m), np.exp(-1 / tau_s)
-     
-    vm = vm * decay_m + const * x.astype(float)
-    vs = vs * decay_s + const * x.astype(float)
-    ref = ref * (1 - sp.astype(float))
-    um = cp.transpose(cp.matmul(w, cp.transpose(vm,(1,0))),(1,0))
-    us = cp.transpose(cp.matmul(w, cp.transpose(vs,(1,0))),(1,0))
-    mem = ref * (um - us)
-    spike = mem > thresh
-    return ref, vm, vs, um, us, mem, spike
+    vm = dvdt.apply(vm, x, tau_m, const)
+    vs = dvdt.apply(vs, x, tau_s, const)
+    sav = sav * (1. - spike)
+    um = ops(vm)
+    us = ops(vs)
+    u = (um - us) * sav
+    spike = dtdu.apply(u, um, us, thresh, tau_m, tau_s)
+
+    if outneuron==False:
+        return sav, vm, vs, um, us, spike
+    return sav, vm, vs, um, us, u, spike
